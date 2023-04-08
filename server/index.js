b5cf008462f5d8a802f165b7b969e8e19a0494ba
Ankit Deb <deb.ankit.work@gmail.com>
2023-04-08 09:00:54 +0530

    initializing server modules

--- server/index.js
+++ server/index.js
@@ -18,5 +18,33 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginEmbedderPolicy({ policy: "cross-origin"}));
+app.use(helmet.crossOriginEmbedderPolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+
+
+//setting Up File Storage
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+
+    }
+});
+
+const upload = multer({ storage });
+
+
+//setting Up Mongoose
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`))
