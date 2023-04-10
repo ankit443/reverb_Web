a6666d53aeea007521baf34e984bbd8d3720a02a
Ankit Deb <deb.ankit.work@gmail.com>
2023-04-10 18:20:56 +0530

    working on logic with redux

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -14,12 +14,44 @@ export const authSlice = createSlice({
     reducers: { //functions that involve modifying the global state
         setMode: (state) => {
             state.mode = state.mode === "light" ? "dark" : "light";
-            //1 hour 58 mins
+        },
+        setLogin: (state, action) => {
+            state.user = action.payload.user; // we are sending a user parameter from this function
 
+            state.token = action.payload.token;
+        },
+        setLogout: (state) => {
+            state.user = null;
+            state.token = null;
 
-            //trying to check the git usage here
-            //checking Git again
+        },
+        setFriends: (state, action) => {
+            if (state.user) {
+                state.user.friends = action.payload.friends;
+
+            }
+            else {
+                console.error("user friends are non existent")
+            }
+        },
+        setPosts: (state, action) => {
+            state.posts = action.payload.posts;
+
+        },
+        setPost: (state, action) => {
+            const updatedPost = state.posts.map((post) => {
+                if (post._id === action.payload.post._id) return action.payload.post;
+                return post;
+            });
+            state.posts = updatedPost;
         }
 
     }
 })
+
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } = authSlice.actions;
+
+export default authSlice.reducer;
+
+//all the logic needed for the application to be done by the redux part is done now
